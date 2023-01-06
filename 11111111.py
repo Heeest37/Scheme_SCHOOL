c6e4e43cdf0024f99d475484529a033f6eb65d92
User <aleks_sobolev3706@mail.ru>
2023-01-06 19:16:47 +0300

    Scheme

--- 11111111.py
+++ 11111111.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import datetime
 
-xls = pd.ExcelFile(r"C:/Users/User/PycharmProjects/PyQTMAPS/Книга1.xlsx") # use r before absolute file path
+xls = pd.ExcelFile(r"C:/Users/User/PycharmProjects/PyQTMAPS/Книга1.xlsx")
 
 now = datetime.datetime.now()
 
-sheetX = xls.parse(0) #2 is the sheet number+1 thus if the file has only 1 sheet write 0 in paranthesis
+sheetX = xls.parse(0)
 
 var1 = sheetX[now.strftime("%A")]
 
